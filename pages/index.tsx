388184f3c2cf24a8823c746d85c562abb4cd3661
Andres Aguirre <aguirregzz97@gmail.com>
2022-07-26 18:41:30 -0700

    🔧 Add prettier and eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div>Hellow World</div>
-  )
-}
+  return <div>Hellow World</div>;
+};
 
-export default Home
+export default Home;
